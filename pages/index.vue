2c017c750825549b20e7668b0b80268bb7607863
Alejandro Cruz <hcruz0716@gmail.com>
2023-01-10 13:33:38 -0600

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-img container"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
