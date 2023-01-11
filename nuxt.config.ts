91e23da1899808b76e0e966c9d9329ba6036d37f
Alejandro Cruz <hcruz0716@gmail.com>
2023-01-11 16:44:06 -0600

    Deploy?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: "static",
+  router: {
+    base: "https://github.com/hdev07/getMore/getMore/",
+  },
   meta: {
     link: [
       {
